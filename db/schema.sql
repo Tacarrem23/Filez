7f4f29327ade679b91ce85252c014a45a07c1d01
Tacarrem23 <carremitchell729@gmail.com>
2025-06-25 21:22:00 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,19 @@
--- TODO
+-- Create the filez database (if it doesn't exist)
+CREATE DATABASE IF NOT EXISTS filez;
+
+-- Switch to the filez database
+\c filez;
+
+CREATE TABLE folders (
+  id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE files (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  size INTEGER NOT NULL,
+  folder_id INTEGER NOT NULL,
+  FOREIGN KEY (folder_id) REFERENCES folders (id) ON DELETE CASCADE,
+  UNIQUE (name, folder_id)
+);
